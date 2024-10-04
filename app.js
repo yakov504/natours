ffdb238e445ca20b58597de2f6078e006b815b3a
yakov504 <yakov0546080824@gmail.com>
2024-10-04 15:36:01 +0300

    env

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const usersRouter = require('./routes/usersRouter');
 const app = express();
 
 /// MiddeleWare ///
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
